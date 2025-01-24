fadafd7cee891753e480188de4ecf970008a6124
Addy768 <adityasharma02092002@gmail.com>
2025-01-24 00:06:25 -0500

    finishing the code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,3 +2,15 @@ import React from "react";
 import Header from "./components/Header";
 import TokenMinting from "./components/TokenMinting";
 import TokenDetails from "./components/TokenDetails";
+
+function App() {
+    return (
+      <div>
+        <Header />
+        <TokenMinting />
+        <TokenDetails />
+      </div>
+    );
+  }
+  
+  export default App;
