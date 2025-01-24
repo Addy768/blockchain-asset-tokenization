675fa78b4345a5c35de77b98913f8797b25cc475
Addy768 <adityasharma02092002@gmail.com>
2025-01-23 19:02:07 -0500

    deployment script for the smart contract using Truffle

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,1 +1,7 @@
 const AssetToken = artifacts.require("AssetToken");
+module.exports = function (deployer) {
+    const name = "AssetToken";
+    const symbol = "AST";
+    const initialSupply = 1000000; // 1 million tokens
+    deployer.deploy(AssetToken, name, symbol, initialSupply);
+  };
