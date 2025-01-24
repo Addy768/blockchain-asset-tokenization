7412698d454a0d1855046bf58308fafc0cc2cc41
Addy768 <adityasharma02092002@gmail.com>
2025-01-24 00:11:35 -0500

    fetching balance

--- frontend/src/components/TokenDetails.js
+++ frontend/src/components/TokenDetails.js
@@ -3,4 +3,8 @@ import React, { useState } from "react";
 function TokenDetails() {
     const [address, setAddress] = useState("");
     const [balance, setBalance] = useState("");
-  
+    const fetchBalance = async () => {
+        const response = await fetch(`http://localhost:5000/balance?address=${address}`);
+        const data = await response.json();
+        setBalance(data.balance);
+      };
